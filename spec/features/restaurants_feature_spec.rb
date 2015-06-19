6d506be99336178b67b32d585d4b3db56d7b359d
Rodney Cullen <rodcul@gmail.com>
2015-06-19 11:45:04 +0100

    Add rating to restaurant

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -3,9 +3,22 @@ require 'rails_helper'
 feature 'restaurants' do
   context 'no restaurants have been added' do
     scenario 'should display a prompt to add a restaurant' do
-      visit '/restaurants'
+      visit '/'
       expect(page).to have_content 'No restaurants yet'
       expect(page).to have_link 'New Restaurant'
+    end
+  end
+
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'KFC', rating: 5)
+    end
+
+    scenario 'display restaurants' do
+      visit '/'
+      expect(page).to have_content('KFC')
+      expect(page).to have_content('5')
+      expect(page).not_to have_content('No restaurants yet')
     end
   end
 end
